c8fc613a37311efc460816a2c992b5dbf4622c2f
Russell Gordon <rgordon@lcs.on.ca>
2024-12-06 13:53:24 -0500

    Initial Commit

---
+++ LeftTurnAheadExample/LeftTurnAheadExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftTurnAheadExampleApp.swift
+//  LeftTurnAheadExample
+//
+//  Created by Russell Gordon on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct LeftTurnAheadExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
