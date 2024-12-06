52385474c219494889d67f86f066dea6d50c32e6
Russell Gordon <rgordon@lcs.on.ca>
2024-12-06 15:30:36 -0500

    Eliminated redundant ZStack.

--- LeftTurnAheadExample/Views/RoadSignView.swift
+++ LeftTurnAheadExample/Views/RoadSignView.swift
@@ -9,46 +9,41 @@ import SwiftUI
 
 struct RoadSignView: View {
     var body: some View {
-        ZStack {
-            // Diamond background
-            DiamondShape()
-                .fill(Color.yellow)
-                .aspectRatio(1.0, contentMode: .fit)
-                .overlay(
-                    GeometryReader { reader in
-                        DiamondShape()
-                            .stroke(
-                                .black,
-                                style: StrokeStyle(
-                                    lineWidth: reader.size.width * 0.02,
-                                    lineCap: .round,
-                                    lineJoin: .round
-                                )
+        DiamondShape()
+            .fill(Color.yellow)
+            .aspectRatio(1.0, contentMode: .fit)
+            .overlay(
+                GeometryReader { reader in
+                    DiamondShape()
+                        .stroke(
+                            .black,
+                            style: StrokeStyle(
+                                lineWidth: reader.size.width * 0.02,
+                                lineCap: .round,
+                                lineJoin: .round
                             )
-                            .padding(reader.size.width * 0.04)
-                            .overlay {
-                                // Left-turn arrow
-                                LeftTurnArrowShape()
-                                    .stroke(
-                                        .black,
-                                        style: StrokeStyle(
-                                            lineWidth: reader.size.width * 0.02,
-                                            lineCap: .round,
-                                            lineJoin: .round
-                                        )
+                        )
+                        .padding(reader.size.width * 0.04)
+                        .overlay {
+                            // Left-turn arrow
+                            LeftTurnArrowShape()
+                                .stroke(
+                                    .black,
+                                    style: StrokeStyle(
+                                        lineWidth: reader.size.width * 0.02,
+                                        lineCap: .round,
+                                        lineJoin: .round
                                     )
-                                    .fill(.black)
-                                    .frame(
-                                        width: reader.size.width * 0.4,
-                                        height: reader.size.width * 0.4
-                                    )
-                                    .offset(x: reader.size.width * -0.05)
-                            }
-                    }
-                )
-                
-            
-        }
+                                )
+                                .fill(.black)
+                                .frame(
+                                    width: reader.size.width * 0.4,
+                                    height: reader.size.width * 0.4
+                                )
+                                .offset(x: reader.size.width * -0.05)
+                        }
+                }
+            )
     }
 }
 
