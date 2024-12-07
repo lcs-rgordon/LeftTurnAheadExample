f68abc4e38fef13d211313ae7fa2133af45607d6
Russell Gordon <rgordon@lcs.on.ca>
2024-12-07 15:32:22 -0500

    Updated app entry point file to load signs example.

--- LeftTurnAheadExample/LeftTurnAheadExampleApp.swift
+++ LeftTurnAheadExample/LeftTurnAheadExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LeftTurnAheadExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ManyRoadSigns()
         }
     }
 }
