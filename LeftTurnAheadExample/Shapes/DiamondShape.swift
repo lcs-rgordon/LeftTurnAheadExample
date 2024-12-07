9aef1cbb7581e6f902c4c59fe41bdabd2d6d6fb6
Russell Gordon <rgordon@lcs.on.ca>
2024-12-07 16:24:23 -0500

    Quad-curve with second reader looks to be a winner?

--- LeftTurnAheadExample/Shapes/DiamondShape.swift
+++ LeftTurnAheadExample/Shapes/DiamondShape.swift
@@ -13,15 +13,27 @@ struct DiamondShape: Shape {
         let w = rect.width
         let h = rect.height
 
-        path.move(to: CGPoint(x: w * 0.48, y: h * 0.02)) // Top point
-        path.addLine(to: CGPoint(x: w * 0.48, y: h * 0.02)) // Top
-        path.addLine(to: CGPoint(x: w * 0.52, y: h * 0.02)) // Top
-        path.addLine(to: CGPoint(x: w * 0.98, y: h * 0.48)) // Right
-        path.addLine(to: CGPoint(x: w * 0.98, y: h * 0.52)) // Right
-        path.addLine(to: CGPoint(x: w * 0.52, y: h * 0.98)) // Bottom
-        path.addLine(to: CGPoint(x: w * 0.48, y: h * 0.98)) // Bottom
-        path.addLine(to: CGPoint(x: w * 0.02, y: h * 0.52)) // Left
-        path.addLine(to: CGPoint(x: w * 0.02, y: h * 0.48)) // Left
+        path.move(to: CGPoint(x: w * 0.4, y: h * 0.1)) // Top point
+        path.addLine(to: CGPoint(x: w * 0.4, y: h * 0.1)) // Top
+        path.addQuadCurve(
+            to: CGPoint(x: w * 0.6, y: h * 0.1),
+            control: CGPoint(x: w * 0.5, y: h * 0.0)    // Control point at apex of curve
+        )
+        path.addLine(to: CGPoint(x: w * 0.9, y: h * 0.4)) // Right
+        path.addQuadCurve(
+            to: CGPoint(x: w * 0.9, y: h * 0.6),
+            control: CGPoint(x: w * 1.0, y: h * 0.5)    // Control point at apex of curve
+        )
+        path.addLine(to: CGPoint(x: w * 0.6, y: h * 0.9)) // Bottom
+        path.addQuadCurve(
+            to: CGPoint(x: w * 0.4, y: h * 0.9),
+            control: CGPoint(x: w * 0.5, y: h * 1.0)    // Control point at apex of curve
+        )
+        path.addLine(to: CGPoint(x: w * 0.1, y: h * 0.6)) // Left
+        path.addQuadCurve(
+            to: CGPoint(x: w * 0.1, y: h * 0.4),
+            control: CGPoint(x: w * 0.0, y: h * 0.5)    // Control point at apex of curve
+        )
         path.closeSubpath()
 
         return path
